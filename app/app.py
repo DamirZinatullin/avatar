a517e4113dcc9e7671a9ca62d0b2c95d1ddc6975
DamirZinatullin <damizinatullin@mail.ru>
2019-11-06 22:28:36 +0300

    Created Users and Roles

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask import redirect, url_for, request
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
+from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+from flask_security import current_user
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -19,12 +22,43 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 ### ADMIN ###
-admin = Admin(app)
-
 from models import *
 
-admin.add_view(ModelView(Suit, db.session))
-admin.add_view(ModelView(Fabric, db.session))
+
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModelView, self).on_model_change(form, model,
+                                                          is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+
+class SuitAdminView(AdminMixin, BaseModelView):
+    form_columns = ['title', 'fabrics', 'price']
+
+
+class FabricAdminView(AdminMixin, BaseModelView):
+    pass
+
+
+admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='Home'))
+admin.add_view(SuitAdminView(Suit, db.session))
+admin.add_view(FabricAdminView(Fabric, db.session))
 
 ### Flask-security ###
 
